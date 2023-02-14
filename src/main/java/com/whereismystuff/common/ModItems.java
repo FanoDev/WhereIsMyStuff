68ba97c59e9fe7dc18d011624bbdf52da6e19a0f
FanoDev <77846470+FanoDev@users.noreply.github.com>
2023-02-14 15:46:24 -0300

    Change the key model and id

--- src/main/java/com/whereismystuff/common/ModItems.java
+++ src/main/java/com/whereismystuff/common/ModItems.java
@@ -9,6 +9,6 @@ public class ModItems {
     public static final Item KEY_ITEM = new KeyItem();
 
     public static void initialize() {
-        Registry.register(Registry.ITEM, new Identifier("whereismystuff", "key_item"), KEY_ITEM);
+        Registry.register(Registry.ITEM, new Identifier("whereismystuff", "key"), KEY_ITEM);
     }
 }
